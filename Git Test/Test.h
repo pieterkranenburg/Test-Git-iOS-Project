0ba3b07b2c17b3a974af0d2f1d477329cc67ae2f
Pieter Kranenburg <pieterkranenburg1@Pieters-Mac-mini.local>
2015-01-21 22:53:37 -0800

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Pieter Kranenburg on 1/21/15.
+//  Copyright (c) 2015 SocialDyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Pieter Kranenburg on 1/21/15.
+//  Copyright (c) 2015 SocialDyn. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
